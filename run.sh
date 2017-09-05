7a5ac40baac0b091862a8f2b2365bedc810ebe2a
Patrick Skjennum <mail@habitats.no>
2017-09-05 11:33:07 +0200

    conf: update run script

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "Indexing..."
+echo "Cleaning up since last run ..."
+rm -r -v wdir
+rm -r -v models
+rm -r -v data
+mkdir -v models
+mkdir -v data
+
+echo "Indexing ..."
 ./index_db.sh -b en/ignore.list wdir en_US en/stopwords.list English   models/en
 ./index_db.sh -b sv/ignore.list wdir sv_SE sv/stopwords.list Swedish   models/sv
 ./index_db.sh -b no/ignore.list wdir no_NO no/stopwords.list Norwegian models/no
