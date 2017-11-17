ccca4dd3af4247e8704ef8cb9ff583ba18ec407c
Patrick Skjennum <mail@habitats.no>
2017-11-17 19:26:28 +0100

    feat: add fr, de, es

--- create_all.sh
+++ create_all.sh
@@ -5,6 +5,9 @@ rm -r -v wdir/en_US
 rm -r -v wdir/no_NO
 rm -r -v wdir/da_DK
 rm -r -v wdir/sv_SE
+rm -r -v wdir/de_DE
+rm -r -v wdir/es_ES
+rm -r -v wdir/fr_FR
 rm -r -v data
 rm -r -v docker/models
 mkdir -v data
@@ -15,9 +18,16 @@ echo "Indexing ..."
 ./index_db.sh -b sv/ignore.list wdir sv_SE sv/stopwords.list Swedish   docker/sv/model
 ./index_db.sh                   wdir da_DK da/stopwords.list Danish    docker/da/model
 ./index_db.sh -b en/ignore.list wdir en_US en/stopwords.list English   docker/en/model
+./index_db.sh -b de/ignore.list wdir de_DE de/stopwords.list German    docker/de/model
+./index_db.sh -b es/ignore.list wdir es_ES es/stopwords.list Spanish   docker/es/model
+./index_db.sh -b fr/ignore.list wdir fr_FR fr/stopwords.list French    docker/fr/model
+
 
 echo "Creating docker images ..."
 docker build --tag mitoai/spotlight-en -f docker/en/Dockerfile docker/en && docker push mitoai/spotlight-en
 docker build --tag mitoai/spotlight-no -f docker/no/Dockerfile docker/no && docker push mitoai/spotlight-no
 docker build --tag mitoai/spotlight-da -f docker/da/Dockerfile docker/da && docker push mitoai/spotlight-da
 docker build --tag mitoai/spotlight-sv -f docker/sv/Dockerfile docker/sv && docker push mitoai/spotlight-sv
+docker build --tag mitoai/spotlight-de -f docker/de/Dockerfile docker/de && docker push mitoai/spotlight-de
+docker build --tag mitoai/spotlight-es -f docker/es/Dockerfile docker/es && docker push mitoai/spotlight-es
+docker build --tag mitoai/spotlight-fr -f docker/fr/Dockerfile docker/fr && docker push mitoai/spotlight-fr
--- prepare.sh
+++ prepare.sh
@@ -14,20 +14,3 @@ else
    echo "One of our dependencies requires Maven 3. Please check your maven version with 'mvn -version'" 
    exit -1
 fi
-
-
-#The current version does not use any OpenNLP models!
-#echo "Downloading OpenNLP models..."
-#for l in en de nl pt da sv; do
-#    cd $l
-#    wget -q http://opennlp.sourceforge.net/models-1.5/$l-token.bin
-#    wget -q http://opennlp.sourceforge.net/models-1.5/$l-sent.bin
-#    wget -q http://opennlp.sourceforge.net/models-1.5/$l-pos-maxent.bin
-#    wget -q http://opennlp.sourceforge.net/models-1.5/$l-chunker.bin
-#    wget -q http://opennlp.sourceforge.net/models-1.5/$l-ner-person.bin
-#    wget -q http://opennlp.sourceforge.net/models-1.5/$l-ner-location.bin
-#    wget -q http://opennlp.sourceforge.net/models-1.5/$l-ner-organization.bin
-#    cd ..
-#done
-
-
